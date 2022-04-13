357f12fa4f500f73262ca72f64ceb04b2fb68015
Kahngjoon Koh <46638829+kahngjoonkoh@users.noreply.github.com>
2022-04-13 09:48:02 +0400

    Update gui.py
    chooses random side for player

--- gui.py
+++ gui.py
@@ -1,14 +1,34 @@
 import random
-
 import chess
 import pygame
 
 
 class App:
-    def __init__(self, debug):
+    def __init__(self, show_info):
         pygame.font.init()
 
-        self.debug = debug
+        self.show_info = show_info
+
+        # CONSTANTS
+        self.FONT_SIZE = 12
+        self.FONT = pygame.font.Font(pygame.font.get_default_font(), self.FONT_SIZE)
+        self.WHITE = (238, 238, 210)
+        self.BLACK = (118, 150, 86)
+        self.TILE_SIZE = 60
+        self.TEXT_MARGIN = 1
+
+        # SCREENS
+        self.screen = pygame.display.set_mode((8 * self.TILE_SIZE, 8 * self.TILE_SIZE))
+        self.chess_grid_surface = pygame.Surface((8 * self.TILE_SIZE, 8 * self.TILE_SIZE))
+
+        # CHESS VARIABLES
+        self.done = False
+
+        self.player_side = None
+        self.tile_on_focus = None
+        self.tile_start = None
+        self.tile_end = None
+        self.pieces = []
 
     def run(self):
         pygame.init()
@@ -18,8 +38,28 @@ class App:
 
         self.board = chess.Board()
 
+        self.initialize_player_side()
+        self.info("Player Side Initialized: White" if self.player_side else "Player Side Initialized: Black")
+
+        self.initialize_chess_grid_surface()
+        self.info("Chess Grid Initialized")
+
+        self.initialize_piece_data()
+        self.info(f"{len(self.pieces)} Pieces Initialised")
+
+        self.refresh_chess_grid_surface()
+        self.info("Main Loop Started")
+
+        while not self.done:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.done = True
+
+            self.refresh_chess_grid_surface()
+            pygame.display.flip()
+
     def initialize_player_side(self):
-        pass
+        self.player_side = random.choice([chess.WHITE, chess.BLACK])
 
     def initialize_chess_grid_surface(self):
         pass
@@ -38,10 +78,11 @@ class App:
         pass
 
 
-    def debug_msg(self, s):
-        if self.debug:
+    def info(self, s):
+        if self.show_info:
             print(s)
 
+
 if __name__ == "__main__":
-    app = App(debug=True)
+    app = App(show_info=True)
     app.run()
